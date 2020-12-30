8d7d859e6be6abac3f8bd880dc303026d564726f
Edvinas Jonusas <ed.jonusas@gmail.com>
2020-12-30 12:29:31 +0200

    first commit

---
+++ components/AddRecept/AddRecept.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Skukurkite receptą</h2>
+    <div>
+      <div>Indegrientai</div>
+      <input type="text" placeholder="produkto pavadinimas" />
+      <input type="text" placeholder="kiekis" />
+      <select>
+        <option value="">Vienetai</option>
+        <option value="">Gramai</option>
+        <option value="">Mililitrai</option>
+      </select>
+      <button>add</button>
+      <button>Delete</button>
+    </div>
+    <div>
+      <label for="description">Recepto aprasymas</label>
+      <textarea id="description"></textarea>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AddRecept',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <add-recept />
+  </div>
+</template>
+
+<script>
+import AddRecept from '~/components/AddRecept/AddRecept.vue'
+export default {
+  components: { AddRecept },
+}
+</script>
