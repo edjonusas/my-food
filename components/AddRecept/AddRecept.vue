041113b2f21d6c757bde967466809e51218880c9
Edvinas Jonusas <ed.jonusas@gmail.com>
2021-01-04 17:59:28 +0200

    AddRecept component, recept form

--- components/AddRecept/AddRecept.vue
+++ components/AddRecept/AddRecept.vue
@@ -1,26 +1,59 @@
 <template>
   <div>
-    <h2>Skukurkite receptą</h2>
-    <div>
-      <div>Indegrientai</div>
-      <input type="text" placeholder="produkto pavadinimas" />
-      <input type="text" placeholder="kiekis" />
-      <select>
-        <option value="">Vienetai</option>
-        <option value="">Gramai</option>
-        <option value="">Mililitrai</option>
-      </select>
-      <button>add</button>
-      <button>Delete</button>
+    <div class="recept-form">
+      <h2>Skukurkite receptą</h2>
+      <div>
+        <div>
+          <label for="recept-title">Recepto pavadinimas</label>
+          <input id="recept-title" v-model="recept.receptTitle" type="text" />
+        </div>
+        <div>Indegrientai</div>
+        <div>
+          <input type="text" placeholder="produkto pavadinimas" />
+          <input type="text" placeholder="kiekis" />
+          <select>
+            <option>Vienetai</option>
+            <option>Gramai</option>
+            <option>Mililitrai</option>
+            <option>Arbatiniai šaukštai</option>
+            <option>Valgomieji šaukštai</option>
+            <option>stiklinės</option>
+          </select>
+        </div>
+
+        <button>Pridėti</button>
+      </div>
+      <div>
+        <label for="description">Recepto aprasymas</label>
+        <textarea id="description"></textarea>
+      </div>
+      <div>
+        <label for="img-url">Recepto nuotraukos url</label>
+        <input id="img-url" type="text" />
+      </div>
     </div>
-    <div>
-      <label for="description">Recepto aprasymas</label>
-      <textarea id="description"></textarea>
+    <div class="recept-preview">
+      <div class="recept-title-preview">{{ recept.receptTitle }}</div>
     </div>
   </div>
 </template>
 <script>
 export default {
   name: 'AddRecept',
+  data() {
+    return {
+      recept: {
+        receptTitle: '',
+        ingredients: [],
+        description: '',
+        imageUrl: '',
+      },
+      ingredient: {
+        name: '',
+        amount: '',
+        unit: '',
+      },
+    }
+  },
 }
 </script>
